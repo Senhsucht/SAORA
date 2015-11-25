c2a22aaa3b9187c40ef0d1fd2f9a976cc6ae5972
Angel Castro <ancastroco@gmail.com>
2015-11-24 20:04:02 -0800

    Navbar terminada, Trabajando en login y recontruyendo formularios con Bootstrap Modulo Inventario aun pendiente

--- SAORA/SAORA/SAORA/apps/Adopcion/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from SAORA.apps.Adopcion.models import *
-
-# Register your models here.
-
-admin.site.register(Ado_Edo)
-admin.site.register(Adoptante)
-admin.site.register(Adopcion)
--- SAORA/SAORA/SAORA/apps/Inventario/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import include, patterns, url
-from django.contrib import admin
-from .views import *
-
-urlpatterns = patterns('',
-	)
